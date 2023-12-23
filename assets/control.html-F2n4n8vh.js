a237f81899874b1f9719710054986065eb1bbd8f
fengyfei <fengyfei@qq.com>
2023-12-23 09:00:19 +0000

    Deploying to gh-pages from @ 2019081/2019081.github.io@3e5427d450bc474a0c173eb2212b9b677ba96a0f 🚀

---
+++ assets/control.html-F2n4n8vh.js
@@ -0,0 +1,16 @@
+import{_ as i}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as e,f as s}from"./app-rZp_wz8U.js";const l="/assets/cplusplus/for-sum-3.jpg",d={},a=s(`<h2 id="_2023-12-23" tabindex="-1"><a class="header-anchor" href="#_2023-12-23" aria-hidden="true">#</a> 2023/12/23</h2><ul><li>求 1 ～ 9 中不是 3 的倍数的所有数字之和</li></ul><div class="language-C++ line-numbers-mode" data-ext="C++"><pre class="language-C++"><code>#include &lt;iostream&gt;
+
+int main() {
+    int sum = 0;
+
+    for (int i = 1; i &lt; 10; i++) {
+        if ((i % 3) != 0) {
+            sum += i;
+        }
+    }
+
+    std::cout &lt;&lt; sum &lt;&lt; std::endl;
+
+    return 0;
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><figure><img src="`+l+'" alt="Execution" tabindex="0" loading="lazy"><figcaption>Execution</figcaption></figure>',4),t=[a];function r(c,u){return n(),e("div",null,t)}const m=i(d,[["render",r],["__file","control.html.vue"]]);export{m as default};
